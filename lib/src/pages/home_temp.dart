76a279a53be8fa30ffc02d64134ab2d72471dad1
AkariHerrera <akari.herrera@cbtis72.edu.mx>
2022-02-22 07:13:13 -0500

    Metodo map de las listas y ListTile

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
   //const ({ Key? key }) : super(key: key);
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +11,39 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[ListTile(title: Text('ListTile Title'))],
-      ),
+          //children: _crearItems()
+          children: _crearItemsCorta()),
     );
+  }
+
+  /* List<Widget> _crearItems() {
+    List<Widget> lista = new List<Wiidget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)
+      ..add(Divider())
+    }
+
+    return lista;
+  }
+*/
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
