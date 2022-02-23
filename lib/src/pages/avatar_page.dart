6e35b3b8324de3859f3cda9b17a538bb2a04cda7
AkariHerrera <akari.herrera@cbtis72.edu.mx>
2022-02-22 23:41:00 -0500

    cambios

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,7 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.blogs.es/85aa44/stan-lee/450_1000.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+          child: FadeInImage(
+        image: NetworkImage(
+            'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/8/87/Stan_Lee.png/revision/latest/top-crop/width/360/height/450?cb=20190303192815&path-prefix=es'),
+        placeholder: AssetImage('assets/jar-loading.gif'),
+        fadeInDuration: Duration(milliseconds: 200),
+      )),
     );
   }
 }
